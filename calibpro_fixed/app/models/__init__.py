ac52a9f28833991924f2ca2c8177914f2c6fe782
ErikLz00 <erick.loayza98@gmail.com>
2026-03-24 02:30:05 -0500

    modificacion para ejecucion 6

--- calibpro_fixed/app/models/__init__.py
+++ calibpro_fixed/app/models/__init__.py
@@ -1,1 +1,1 @@
-from app.models.database import query, execute, executemany, row_to_dict, rows_to_list
+from calibpro_fixed.app.models.database import query, execute, executemany, row_to_dict, rows_to_list
